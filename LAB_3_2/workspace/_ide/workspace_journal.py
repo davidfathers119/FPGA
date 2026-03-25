bed2f97853aabac0acd2d410c1507042abc6b513
Dave Hung <davehung@base.alltekmarine.com>
2026-03-25 13:07:06 +0800

    Initial commit for LAB3_2

---
+++ LAB_3_2/workspace/_ide/workspace_journal.py
@@ -0,0 +1,38 @@
+# 2026-03-25T11:54:21.082293800
+import vitis
+
+client = vitis.create_client()
+client.set_workspace(path="workspace")
+
+platform = client.create_platform_component(name = "platform",hw_design = "$COMPONENT_LOCATION/../../../project_1/design_1_wrapper.xsa",os = "standalone",cpu = "ps7_cortexa9_0",domain_name = "standalone_ps7_cortexa9_0")
+
+comp = client.create_app_component(name="hello_world",platform = "$COMPONENT_LOCATION/../platform/export/platform/platform.xpfm",domain = "standalone_ps7_cortexa9_0",template = "hello_world")
+
+platform = client.get_component(name="platform")
+status = platform.build()
+
+status = platform.build()
+
+comp = client.get_component(name="hello_world")
+comp.build()
+
+status = platform.update_hw(hw_design = "$COMPONENT_LOCATION/../../../project_1/design_1_wrapper.xsa")
+
+status = platform.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
---
+++ LAB_3_2/workspace/platform/zynq_fsbl/zynq_fsbl_bsp/libsrc/build_configs/gen_bsp/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,19 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
+  file(REMOVE_RECURSE
+  "C:\\FPGA\\CHU_FPGA_LAB\\LAB_3_2\\LAB_3_2\\workspace\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\diskio.h"
+  "C:\\FPGA\\CHU_FPGA_LAB\\LAB_3_2\\LAB_3_2\\workspace\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\ff.h"
+  "C:\\FPGA\\CHU_FPGA_LAB\\LAB_3_2\\LAB_3_2\\workspace\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\ffconf.h"
+  "C:\\FPGA\\CHU_FPGA_LAB\\LAB_3_2\\LAB_3_2\\workspace\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\sleep.h"
+  "C:\\FPGA\\CHU_FPGA_LAB\\LAB_3_2\\LAB_3_2\\workspace\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xilffs.h"
+  "C:\\FPGA\\CHU_FPGA_LAB\\LAB_3_2\\LAB_3_2\\workspace\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xilffs_config.h"
+  "C:\\FPGA\\CHU_FPGA_LAB\\LAB_3_2\\LAB_3_2\\workspace\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xilrsa.h"
+  "C:\\FPGA\\CHU_FPGA_LAB\\LAB_3_2\\LAB_3_2\\workspace\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xiltimer.h"
+  "C:\\FPGA\\CHU_FPGA_LAB\\LAB_3_2\\LAB_3_2\\workspace\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xtimer_config.h"
+  "C:\\FPGA\\CHU_FPGA_LAB\\LAB_3_2\\LAB_3_2\\workspace\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\lib\\libxilffs.a"
+  "C:\\FPGA\\CHU_FPGA_LAB\\LAB_3_2\\LAB_3_2\\workspace\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\lib\\libxilrsa.a"
+  "C:\\FPGA\\CHU_FPGA_LAB\\LAB_3_2\\LAB_3_2\\workspace\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\lib\\libxiltimer.a"
+  )
+endif()
